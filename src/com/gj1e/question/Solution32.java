51265c7444cebff2b38833da2067222c0240340e
GJ1e <825843207@qq.com>
2020-03-03 13:05:42 +0800

    更新剑指offer第32题，第33题

--- src/com/gj1e/question/Solution32.java
+++ src/com/gj1e/question/Solution32.java
@@ -1,9 +1,42 @@
 package com.gj1e.question;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * @Author GJ1e
  * @Create 2020/3/3
  * @Time 9:21
+ * 面试题32：从上到下打印二叉树
+ *
+ * 题目：
+ *      从上往下打印出二叉树的每个节点，同层节点从左至右打印。
+ * 思路：
+ *      1.创建一个辅助队列，每遍历一个节点，则判断该节点是否有左右子节点，若有则加入队列。然后将队首节点出队列。
+ *      2.继续按上述操作遍历队列中的节点，直到队列为空。
  */
 public class Solution32 {
+    //定义二叉树节点
+    class BinaryTreeNode{
+        int value;
+        BinaryTreeNode left = null;
+        BinaryTreeNode right = null;
+    }
+
+    public ArrayList<Integer> PrintFromTopToBottom (BinaryTreeNode root){
+        if (root == null)
+            return null;
+        ArrayList<Integer> list = new ArrayList<>();
+        Queue<BinaryTreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()){
+            if (queue.peek().left != null)  //若队首节点有左子节点，则将左子节点入队列
+                queue.offer(queue.peek().left);
+            if (queue.peek().right != null) //若队首节点有有子节点，则将右子节点入队列
+                queue.offer(queue.peek().right);
+            list.add(queue.poll().value);   //将队首元素出队列。
+        }
+        return list;
+    }
 }
--- src/com/gj1e/question/Solution33.java
+++ src/com/gj1e/question/Solution33.java
@@ -4,6 +4,37 @@ package com.gj1e.question;
  * @Author GJ1e
  * @Create 2020/3/3
  * @Time 12:13
+ * 面试题33：二叉搜索树的后序遍历序列
+ *
+ * 题目：
+ *      输入一个整数数组，判断该数组是不是某二叉搜索树的后序遍历的结果。如果是则输出Yes,否则输出No。
+ *      假设输入的数组的任意两个数字都互不相同。
+ *
+ * 思路：
+ *      利用二叉搜索树的特性，不断递归判断，左右子树是否符合标准。左右子树必须同时符合二叉搜索树的特点才行。
  */
 public class Solution33 {
+    public boolean verifySquenceOfBST(int[] sequence) {
+        if (sequence==null || sequence.length<=0)
+            return false;
+        return isTreeBST(sequence,0,sequence.length-1);
+    }
+
+    private boolean isTreeBST(int[] sequence, int start, int end){
+        // start比end大说明上层结点没有左子树或者右子树，begin == end说明该本层结点没有子树，无需比较了
+        // 这两种情况都应该返回true
+        if(start >= end) return true;
+        int i = start;
+        for (;i < end; i++){
+            if (sequence[i]>sequence[end])  //左子树都比root小
+                break;
+        }
+        for (int j = i;j < end; j++){
+            if (sequence[j]<sequence[end])  //右子树中还存在比root小的说明不是二叉搜索树
+                return false;
+        }
+        //左右子树必须同时都是二叉搜索树
+        return isTreeBST(sequence,start,i-1) && isTreeBST(sequence,i,end-1);
+
+    }
 }
